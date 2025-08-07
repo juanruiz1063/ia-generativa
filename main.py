e3a25c7945e1dfd4f18c078ab71693570f9d0ef6
juanruiz1063@github.com <juan.ruiz@satrack.com>
2025-08-07 11:59:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain import PromptTemplate
+#from langchain import PromptTemplate
 from langchain_openai import OpenAI
 from langchain.chains.summarize import load_summarize_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
